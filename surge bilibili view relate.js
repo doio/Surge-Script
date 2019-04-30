88ba2d8659cd62a17cab7a5e38541cf53838915f
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-04-30 19:36:02 +0800

    Create surge bilibili view relate.js

---
+++ surge bilibili view relate.js
@@ -0,0 +1,14 @@
+var obj = JSON.parse(body); 
+
+obj1=obj['data']['relates'];
+
+obj1.forEach(function (element, index, array) {
+   
+    if(element.['is_ad']=="true"){      
+       obj1.splice(index,1)  
+    }
+
+   });
+
+
+JSON.stringify(obj);
