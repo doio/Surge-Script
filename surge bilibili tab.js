589cead6812971769c78f807dd7e485271f7e543
Way Away <37437616+onewayticket255@users.noreply.github.com>
2019-05-02 21:01:33 +0800

    Update surge bilibili tab.js

--- surge bilibili tab.js
+++ surge bilibili tab.js
@@ -4,19 +4,22 @@ var obj = JSON.parse(body);
 obj1=obj['data']['tab'];
 obj2=obj['data']['bottom'];
 
+
+obj1.forEach(function (element, index, array) {
+  
+    if(element['name']!="追番"&&element['name']!="推荐"&&element['name']!="直播"){      
+       obj1.splice(index,1)  
+    }
+
+   });
+
 obj2.forEach(function (element, index, array) {
     if(element['pos']==4){      
        obj2.splice(index,1)  
     }
 
    });
-obj1.forEach(function (element, index, array) {
-  
-    if(element['id']!=99&&element['id']!=101&&element['id']!=98){      
-       obj1.splice(index,1)  
-    }
 
-   });
 
 obj['data']['tab']=obj1
 obj['data']['bottom']=obj2
